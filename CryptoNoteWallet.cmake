32e39d48495520bd4128ddb9b97253670cccee38
stealthgerbil <34917613+stealthgerbil@users.noreply.github.com>
2018-07-15 15:21:58 -0400

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "pegglecoin")
+set(CN_CURRENCY_DISPLAY_NAME "Peggle Coin")
+set(CN_CURRENCY_TICKER "Peggle")
